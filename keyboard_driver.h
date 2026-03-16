6d4438c6c2b481b2bb074dca580bf8dd9a2b3656
kqueej <alexandrd542@gmail.com>
2026-03-16 07:27:12 +0300

    Update keyboard_driver.h

--- keyboard_driver.h
+++ keyboard_driver.h
@@ -4,10 +4,10 @@
 #include <stddef.h>
 #include <stdint.h>
 
-// Инициализация клавиатуры
+
 void keyboard_initialize(void);
 
-// Считывание символов с клавиатуры
+
 char keyboard_read(void);
 
-#endif // KEYBOARD_DRIVER_H
+#endif
